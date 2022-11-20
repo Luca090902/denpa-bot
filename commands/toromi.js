6d6fc872baa9077c16a5e06668e827997b5c984b
Killbunny <killbunny10@gmail.com>
2022-11-19 18:04:26 -0600

    added toromi command

---
+++ commands/toromi.js
@@ -0,0 +1,25 @@
+const config = require('./../config.json');
+const ytfps = require('ytfps');
+module.exports = {
+    name: 'toromi',
+    aliases: [';;t'],
+    inVoiceChannel: true,
+    run: async (client, message) => {
+        var string = '';
+        ytfps(config.tplaylist).then(playlist => {
+            var songurl = playlist.videos[randomIntFromInterval(0, playlist.videos.length - 1)].url;
+            console.log(songurl);
+            string = songurl;
+            client.distube.play(message.member.voice.channel, string, {
+                member: message.member,
+                textChannel: message.channel,
+                metadata: message,
+                message
+            })
+        });
+
+    }
+}
+function randomIntFromInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
