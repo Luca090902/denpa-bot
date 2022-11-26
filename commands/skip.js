e1b054aaed39db268cbef68ccd65a92c467499c0
Killbunny <killbunny10@gmail.com>
2022-11-25 21:03:54 -0600

    skip from the end

--- commands/skip.js
+++ commands/skip.js
@@ -14,6 +14,7 @@ module.exports = {
           var rEnd = argsRange[1]
           await queue._taskQueue.queuing();
           try {
+            rEnd = rEnd >= queue.songs.length ? queue.songs.length - 1 : rEnd;
             var skippedsongs = queue.songs.splice(rBegin, rEnd - rBegin + 1);
             message.channel.send(`${client.emotes.success} | Skipped songs ${rBegin} to ${rEnd}`)
           } finally {
@@ -27,12 +28,18 @@ module.exports = {
       else {
         await queue._taskQueue.queuing();
         try {
-          if (arg >= queue.songs.length) {
-            message.channel.send(`${client.emotes.error} | Not found. No song has been skipped.`)
+          if (arg < 0) {
+            var skippedsongs = queue.songs.splice(arg);
+            message.channel.send(`${client.emotes.success} | Skipped song ${Math.abs(arg)} from the end of the playlist`)
           } else {
-            var skippedsong = queue.songs.splice(arg, 1);
-            message.channel.send(`${client.emotes.success} | Skipped song ${skippedsong[0].name}`)
+            if (arg >= queue.songs.length) {
+              message.channel.send(`${client.emotes.error} | Not found. No song has been skipped.`)
+            } else {
+              var skippedsong = queue.songs.splice(arg, 1);
+              message.channel.send(`${client.emotes.success} | Skipped song ${skippedsong[0].name}`)
+            }
           }
+
         } finally {
           queue._taskQueue.resolve();
         }
