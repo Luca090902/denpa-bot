29ab302504e39318da99037bc6a01029b61a814f
Killbunny <killbunny10@gmail.com>
2023-01-08 16:23:58 -0600

    removed console logs from roles

--- commands/role.js
+++ commands/role.js
@@ -8,12 +8,12 @@ module.exports = {
       let msg = ''
       if (args.length === 0) {
         // No args, list
-        message.guild.roles.cache.forEach(role => console.log(role.name, role.id))
+        // message.guild.roles.cache.forEach(role => console.log(role.name, role.id))
         msg = message.guild.roles.cache.map((role, i) => `${role.name}`).join(', ')
       } else if (args.length > 0) {
         const role = args.join(' ')
         await message.member.roles.add(message.member.guild.roles.cache.find(i => i.name === role))
-        console.log(`self role ${role} added to ${message.member.user.username}`)
+        // console.log(`self role ${role} added to ${message.member.user.username}`)
         msg = `${client.emotes.denpabot} | ${message.member.user.username} established connection with ${role}`
       }
       const queueEmbed = new Discord.EmbedBuilder()
