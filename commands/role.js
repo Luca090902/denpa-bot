b5c02e0868a1a35eae04b2bdb90f85f57e836b99
Delta <editorrus2@gmail.com>
2023-01-12 23:10:02 +0500

    Lint

--- commands/role.js
+++ commands/role.js
@@ -7,101 +7,109 @@ module.exports = {
   aliases: ['broadcast', 'syntonize', 'roles'],
   run: async (client, message, args) => {
     try {
-      const hasPermissions = message.member.permissions.has(Discord.PermissionsBitField.Flags.Administrator)
-        || message.member.permissions.has(Discord.PermissionsBitField.Flags.ManageRoles);
+      const hasPermissions =
+        message.member.permissions.has(Discord.PermissionsBitField.Flags.Administrator) ||
+        message.member.permissions.has(Discord.PermissionsBitField.Flags.ManageRoles)
 
       let msg = ''
       if (args.length === 0) {
         // No args, list
 
-        let roles = message.guild.roles.cache.map(role => role.name);
-        roles = (!hasPermissions) ? roles.filter(name => !Dmpcfg.roleblacklist.includes(name)) : roles;
+        let roles = message.guild.roles.cache.map(role => role.name)
+        roles = !hasPermissions ? roles.filter(name => !Dmpcfg.roleblacklist.includes(name)) : roles
 
-        msg = roles.join(", ")
+        msg = roles.join(', ')
       } else if (args.length > 0) {
         const roleSelected = args.length > 1 ? args.splice(1, args.length).join(' ') : args.join(' ')
 
         // https://stackoverflow.com/questions/40999025/javascript-scope-variable-to-switch-case (lmao)
         switch (args[0]) {
-          case 'remove': {
-            const found = findRole(message.member.guild.roles, roleSelected)
+          case 'remove':
+            {
+              const found = findRole(message.member.guild.roles, roleSelected)
 
-            // Anti-IDoApologise measure
-            if (Dmpcfg.roleblacklist.includes(found.name)) {
-              msg = `${client.emotes.error} | Not allowed`
-              break
+              // Anti-IDoApologise measure
+              if (Dmpcfg.roleblacklist.includes(found.name)) {
+                msg = `${client.emotes.error} | Not allowed`
+                break
+              }
+
+              await message.member.roles.remove(found)
+
+              msg = `${client.emotes.denpabot} | ${message.member.user.username} is no longer in tune with ${found.name}`
             }
-
-            await message.member.roles.remove(found)
-
-            msg = `${client.emotes.denpabot} | ${message.member.user.username} is no longer in tune with ${found.name}`
-          } break;
+            break
 
           case 'blacklist':
-          case 'bl': {
-            if (roleSelected === "bl" || roleSelected === "blacklist") {
-              msg = Dmpcfg.roleblacklist.map(role => `${role}`).join(', ')
-              break
-            }
-
-            if (!hasPermissions) {
-              msg = `${client.emotes.error} | Not allowed`
-              break
-            }
-
-            const found = findRole(message.member.guild.roles, roleSelected)
-
-            if (found !== undefined) {
-              if (Dmpcfg.roleblacklist.includes(found.name)) {
-                msg = `${client.emotes.error} | Already blacklisted`
-                break;
+          case 'bl':
+            {
+              if (roleSelected === 'bl' || roleSelected === 'blacklist') {
+                msg = Dmpcfg.roleblacklist.map(role => `${role}`).join(', ')
+                break
               }
 
-              Dmpcfg.roleblacklist.push(found.name)
+              if (!hasPermissions) {
+                msg = `${client.emotes.error} | Not allowed`
+                break
+              }
 
-              fs.writeFileSync(PATH, JSON.stringify(Dmpcfg, null, '\t'))
-              msg = `${client.emotes.denpabot} | blacklisted role ${found.name}`
-            } else {
-              msg = `${client.emotes.error} | Role doesn't exist`
-            }
-          } break;
+              const found = findRole(message.member.guild.roles, roleSelected)
 
-          case 'unblacklist':
-          case 'unbl': {
-            if (!hasPermissions) {
-              msg = `${client.emotes.error} | Not allowed`
-              break;
-            }
+              if (found !== undefined) {
+                if (Dmpcfg.roleblacklist.includes(found.name)) {
+                  msg = `${client.emotes.error} | Already blacklisted`
+                  break
+                }
 
-            const found = findRole(message.member.guild.roles, roleSelected)
-
-            if (found !== undefined) {
-              if (Dmpcfg.roleblacklist.includes(found.name)) {
-                Dmpcfg.roleblacklist = Dmpcfg.roleblacklist.filter(e => e !== found.name)
+                Dmpcfg.roleblacklist.push(found.name)
 
                 fs.writeFileSync(PATH, JSON.stringify(Dmpcfg, null, '\t'))
-                msg = `${client.emotes.denpabot} | unblacklisted role ${found.name}`
+                msg = `${client.emotes.denpabot} | blacklisted role ${found.name}`
               } else {
-                msg = `${client.emotes.error} | Not blacklisted`
+                msg = `${client.emotes.error} | Role doesn't exist`
               }
-            } else {
-              msg = `${client.emotes.error} | Role doesn't exist`
             }
+            break
 
-          } break;
+          case 'unblacklist':
+          case 'unbl':
+            {
+              if (!hasPermissions) {
+                msg = `${client.emotes.error} | Not allowed`
+                break
+              }
+
+              const found = findRole(message.member.guild.roles, roleSelected)
+
+              if (found !== undefined) {
+                if (Dmpcfg.roleblacklist.includes(found.name)) {
+                  Dmpcfg.roleblacklist = Dmpcfg.roleblacklist.filter(e => e !== found.name)
+
+                  fs.writeFileSync(PATH, JSON.stringify(Dmpcfg, null, '\t'))
+                  msg = `${client.emotes.denpabot} | unblacklisted role ${found.name}`
+                } else {
+                  msg = `${client.emotes.error} | Not blacklisted`
+                }
+              } else {
+                msg = `${client.emotes.error} | Role doesn't exist`
+              }
+            }
+            break
 
           case 'add':
-          default: {
-            const found = findRole(message.member.guild.roles, roleSelected)
-            await message.member.roles.add(found)
+          default:
+            {
+              const found = findRole(message.member.guild.roles, roleSelected)
+              await message.member.roles.add(found)
 
-            // TODO: Add ability for an admin to add a role this way? 
-            if (!Dmpcfg.roleblacklist.includes(found.name)) {
-              msg = `${client.emotes.denpabot} | ${message.member.user.username} established connection with ${found.name}`
-            } else {
-              msg = `${client.emotes.error} | Not allowed`
+              // TODO: Add ability for an admin to add a role this way?
+              if (!Dmpcfg.roleblacklist.includes(found.name)) {
+                msg = `${client.emotes.denpabot} | ${message.member.user.username} established connection with ${found.name}`
+              } else {
+                msg = `${client.emotes.error} | Not allowed`
+              }
             }
-          } break;
+            break
         }
       }
       const queueEmbed = new Discord.EmbedBuilder()
--- commands/skip.js
+++ commands/skip.js
@@ -5,38 +5,39 @@ module.exports = {
   run: async (client, message, args) => {
     const queue = client.distube.getQueue(message)
     if (!queue) return message.channel.send(`${client.emotes.error} | There is nothing in the queue right now!`)
-    var arg = args.join();
+    const arg = args.join()
     try {
-      if (isNaN(arg) || args == '') {
+      if (isNaN(arg) || args === '') {
         if (arg.includes('-')) {
           // SKIP BY RANGE
-          var argsRange = arg.split('-')
-          var rBegin = argsRange[0]
-          var rEnd = argsRange[1]
-          await queue._taskQueue.queuing();
+          const argsRange = arg.split('-')
+          const rBegin = argsRange[0]
+          let rEnd = argsRange[1]
+          await queue._taskQueue.queuing()
           try {
-            rEnd = rEnd >= queue.songs.length ? queue.songs.length - 1 : rEnd;
-            var skippedsongs = queue.songs.splice(rBegin, rEnd - rBegin + 1);
+            rEnd = rEnd >= queue.songs.length ? queue.songs.length - 1 : rEnd
+            queue.songs.splice(rBegin, rEnd - rBegin + 1)
             message.channel.send(`${client.emotes.success} | Skipped songs ${rBegin} to ${rEnd}`)
           } finally {
-            queue._taskQueue.resolve();
+            queue._taskQueue.resolve()
           }
         } else {
           // SKIP (NO ARG)
           const song = await queue.skip()
           message.channel.send(`${client.emotes.success} | Skipped! Now playing:\n${song.name}`)
         }
-      }
-      else {
-        await queue._taskQueue.queuing();
+      } else {
+        await queue._taskQueue.queuing()
         try {
           if (arg < 0) {
             // SKIP BY LAST SONG
             if (Math.abs(arg) >= queue.songs.length) {
               message.channel.send(`${client.emotes.error} | Not found. No song has been skipped.`)
             } else {
-              var skippedsongs = queue.songs.splice(arg);
-              message.channel.send(`${client.emotes.success} | Skipped song ${Math.abs(arg)} from the end of the playlist`)
+              queue.songs.splice(arg)
+              message.channel.send(
+                `${client.emotes.success} | Skipped song ${Math.abs(arg)} from the end of the playlist`
+              )
             }
           } else {
             if (arg >= queue.songs.length) {
@@ -44,13 +45,12 @@ module.exports = {
               message.channel.send(`${client.emotes.error} | Not found. No song has been skipped.`)
             } else {
               // SKIP BY INDEX
-              var skippedsong = queue.songs.splice(arg, 1);
+              const skippedsong = queue.songs.splice(arg, 1)
               message.channel.send(`${client.emotes.success} | Skipped song ${skippedsong[0].name}`)
             }
           }
-
         } finally {
-          queue._taskQueue.resolve();
+          queue._taskQueue.resolve()
         }
       }
     } catch (e) {
