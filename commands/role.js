d8e75f000e5bdb3f0adbc8c6165d535dd52ea0a5
attosizemir <choko_matte@protonmail.com>
2023-01-12 00:45:19 -0600

    fix: interacting with roles is now case-insensitive

--- commands/role.js
+++ commands/role.js
@@ -10,69 +10,90 @@ module.exports = {
       let msg = ''
       if (args.length === 0) {
         // No args, list
-        // message.guild.roles.cache.forEach(role => console.log(role.name, role.id))
-        msg = message.guild.roles.cache.map((role, i) => `${role.name}`).join(', ')
+        msg = message.guild.roles.cache.map(role => `${role.name}`).join(', ')
       } else if (args.length > 0) {
         const roleSelected = args.length > 1 ? args.splice(1, args.length).join(' ') : args.join(' ')
+
         switch (args[0]) {
-          case 'remove':
-            await message.member.roles.remove(message.member.guild.roles.cache.find(i => i.name === roleSelected))
-            msg = `${client.emotes.denpabot} | ${message.member.user.username} is no longer in tune with ${roleSelected}`
-            break
+          case 'remove': {
+            // https://stackoverflow.com/questions/40999025/javascript-scope-variable-to-switch-case (lmao)
+            const found = findRole(message.member.guild.roles, roleSelected)
+            await message.member.roles.remove(found)
+
+            msg = `${client.emotes.denpabot} | ${message.member.user.username} is no longer in tune with ${found.name}`
+          } break;
+
           case 'blacklist':
-          case 'bl':
-            if (roleSelected === 'bl' || roleSelected === 'blacklist') {
-              msg = Dmpcfg.roleblacklist.map((role, i) => `${role}`).join(', ')
-            } else if (
-              message.member.permissions.has(Discord.PermissionsBitField.Flags.Administrator) ||
-              message.member.permissions.has(Discord.PermissionsBitField.Flags.ManageRoles)
-            ) {
-              if (message.member.guild.roles.cache.find(i => i.name === roleSelected) > 0) {
-                if (!Dmpcfg.roleblacklist.includes(roleSelected)) {
-                  Dmpcfg.roleblacklist.push(roleSelected)
-                  fs.writeFileSync(PATH, JSON.stringify(Dmpcfg, null, '\t'))
-                  msg = `${client.emotes.denpabot} | blacklisted role ${roleSelected}`
-                } else {
-                  msg = `${client.emotes.error} | Already blacklisted`
-                }
-              } else {
-                msg = `${client.emotes.error} | Role doesn't exists`
-              }
-            } else {
+          case 'bl': {
+            const hasPermissions = message.member.permissions.has(Discord.PermissionsBitField.Flags.Administrator)
+              || message.member.permissions.has(Discord.PermissionsBitField.Flags.ManageRoles)
+
+            if (!hasPermissions) {
               msg = `${client.emotes.error} | Not allowed`
-            }
-            break
-          case 'unblacklist':
-          case 'unbl':
-            if (
-              message.member.permissions.has(Discord.PermissionsBitField.Flags.Administrator) ||
-              message.member.permissions.has(Discord.PermissionsBitField.Flags.ManageRoles)
-            ) {
-              if (message.member.guild.roles.cache.find(i => i.name === roleSelected) > 0) {
-                if (Dmpcfg.roleblacklist.includes(roleSelected)) {
-                  Dmpcfg.roleblacklist = Dmpcfg.roleblacklist.filter(e => e !== roleSelected)
-                  fs.writeFileSync(PATH, JSON.stringify(Dmpcfg, null, '\t'))
-                  msg = `${client.emotes.denpabot} | unblacklisted role ${roleSelected}`
-                } else {
-                  msg = `${client.emotes.error} | Not blacklisted`
-                }
-              } else {
-                msg = `${client.emotes.error} | Role doesn't exists`
-              }
-            } else {
-              msg = `${client.emotes.error} | Not allowed`
+              break
             }
 
-            break
+            if (roleSelected === "bl" || roleSelected === "blacklist") {
+              msg = Dmpcfg.roleblacklist.map(role => `${role}`).join(', ')
+              break
+            }
+
+            const found = findRole(message.member.guild.roles, roleSelected)
+
+            if (found !== undefined) {
+              if (Dmpcfg.roleblacklist.includes(found.name)) {
+                msg = `${client.emotes.error} | Already blacklisted`
+                break;
+              }
+
+              Dmpcfg.roleblacklist.push(found.name)
+
+              fs.writeFileSync(PATH, JSON.stringify(Dmpcfg, null, '\t'))
+              msg = `${client.emotes.denpabot} | blacklisted role ${found.name}`
+            } else {
+              msg = `${client.emotes.error} | Role doesn't exist`
+            }
+          } break;
+
+          case 'unblacklist':
+          case 'unbl': {
+            const hasPermissions = message.member.permissions.has(Discord.PermissionsBitField.Flags.Administrator)
+              || message.member.permissions.has(Discord.PermissionsBitField.Flags.ManageRoles)
+
+            if (!hasPermissions) {
+              msg = `${client.emotes.error} | Not allowed`
+              break;
+            }
+
+            const found = findRole(message.member.guild.roles, roleSelected)
+
+            if (found !== undefined) {
+              if (Dmpcfg.roleblacklist.includes(found.name)) {
+                Dmpcfg.roleblacklist = Dmpcfg.roleblacklist.filter(e => e !== found.name)
+
+                fs.writeFileSync(PATH, JSON.stringify(Dmpcfg, null, '\t'))
+                msg = `${client.emotes.denpabot} | unblacklisted role ${found.name}`
+              } else {
+                msg = `${client.emotes.error} | Not blacklisted`
+              }
+            } else {
+              msg = `${client.emotes.error} | Role doesn't exist`
+            }
+
+          } break;
+
           case 'add':
-          default:
-            if (!Dmpcfg.roleblacklist.includes(roleSelected)) {
-              await message.member.roles.add(message.member.guild.roles.cache.find(i => i.name === roleSelected))
-              msg = `${client.emotes.denpabot} | ${message.member.user.username} established connection with ${roleSelected}`
+          default: {
+            const found = findRole(message.member.guild.roles, roleSelected)
+            await message.member.roles.add(found)
+
+            // TODO: Add ability for an admin to add a role this way? 
+            if (!Dmpcfg.roleblacklist.includes(found.name)) {
+              msg = `${client.emotes.denpabot} | ${message.member.user.username} established connection with ${found.name}`
             } else {
               msg = `${client.emotes.error} | Not allowed`
             }
-            break
+          } break;
         }
       }
       const queueEmbed = new Discord.EmbedBuilder()
@@ -83,7 +104,13 @@ module.exports = {
 
       message.channel.send({ embeds: [queueEmbed] })
     } catch (e) {
-      message.channel.send(`${client.emotes.error} | Frequency out of range`)
+      message.channel.send(`${client.emotes.error} | Frequency out of range: ${e}`)
     }
   }
+}
+
+/** case-insensitive role search */
+const findRole = (roles, needle) => {
+  const needleLower = needle.toLowerCase()
+  return roles.cache.find(r => r.name.toLowerCase() === needleLower)
 }
