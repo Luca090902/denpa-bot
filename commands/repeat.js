fc63506ea84cf784c9879cc5a940c8780b40da7d
attosizemir <choko_matte@protonmail.com>
2023-03-01 23:12:49 -0600

    chore: lint

--- commands/repeat.js
+++ commands/repeat.js
@@ -5,17 +5,17 @@ module.exports = {
   run: async (client, message, args) => {
     const queue = client.distube.getQueue(message)
     if (!queue) return message.channel.send(`${client.emotes.error} | There is nothing playing!`)
-    let mode = null;
+    let mode = null
     switch (args[0]) {
       case 'off':
         mode = 0
         break
+      case 'queue':
+        mode = 2
+        break
       case 'song':
       default:
         mode = 1
-        break
-      case 'queue':
-        mode = 2
         break
     }
     mode = queue.setRepeatMode(mode)
