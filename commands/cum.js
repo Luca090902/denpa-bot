773048a26d48f48f204ae6ea25f3bf8e57bb28f1
LypTea <randomly132@gmail.com>
2023-09-15 22:11:25 -0500

    wip

---
+++ commands/cum.js
@@ -0,0 +1,10 @@
+module.exports = {
+  name: 'dcount',
+  aliases: [],
+  inVoiceChannel: false,
+  run: async (client, message, args) => {
+    console.log(args)
+
+    // message.channel.send
+  }
+}
